4de9c46820c8935709961cb7cfd2f652fd5d6b70
pten <pten@futurice.com>
2018-08-06 13:21:25 +0300

    Add links to share Add server to server

--- server/flask_app.py
+++ server/flask_app.py
@@ -2,10 +2,12 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
-
+from flask_cors import cross_origin
+from server import get_open_graph
 app = Flask(__name__)
 
 @app.route('/')
+@cross_origin()
 def hello_world():
-    return 'Hello from Flask!'
+    return get_open_graph()
 
---
+++ server/server.py
@@ -0,0 +1,10 @@
+import urllib3
+import os
+og_key = os.environ("OPENGRAPH_APIKEY")
+http = urllib3.PoolManager()
+
+
+def get_open_graph():
+    url = "https://opengraph.io/api/1.1/site/http://www.futurice.com"+"?app_id="+og_key
+    r = http.request('GET', url)
+    return r.data
