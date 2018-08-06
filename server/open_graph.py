1dd7f190eaa2bb70c8b6978484b0fad3feb0300f
pten <pten@futurice.com>
2018-08-06 14:26:25 +0300

    rename server to open_graph for clarity and fixed proper encoding of url

--- server/open_graph.py
+++ server/open_graph.py
@@ -1,4 +1,5 @@
 import urllib3
+from urllib.parse import quote
 import os
 og_key = os.environ["OPENGRAPH_APIKEY"]
 
@@ -6,6 +7,7 @@ http = urllib3.PoolManager()
 
 
 def get_open_graph():
-    url = "https://opengraph.io/api/1.1/site/http://www.futurice.com"+"?app_id="+og_key
+    url = "https://opengraph.io/api/1.1/site/" + quote("http://www.futurice.com") + "?app_id="+og_key
+    print(url)
     r = http.request('GET', url)
     return r.data
