b69dc13fdebddd96d758e639d4cbf42d2753e931
pten <pten@futurice.com>
2018-08-17 15:31:53 +0300

    add basic cache

--- server/flask_app.py
+++ server/flask_app.py
@@ -5,15 +5,20 @@ from flask import Flask
 from flask_cors import cross_origin
 from open_graph import get_open_graph
 from flask import request
+from werkzeug.contrib.cache import FileSystemCache
 
 app = Flask(__name__)
-
+cache = FileSystemCache('/var/tmp/OpenGraphhCahche')
 @app.route('/')
 @cross_origin()
 def hello_world():
     url = request.args.get('url')
     if url:
-        return get_open_graph(url)
+        response = cache.get(url)
+        if response is None:
+            response = get_open_graph(url)
+            cache.set(url, response, timeout=60*60)
+        return response
     else:
         return "error"
 
